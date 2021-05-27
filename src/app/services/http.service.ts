75c0e1aa274d0c5a6b72ac9d9cda03d4ea4b5b7e
gautamb97 <www.gautam971997@gmail.com>
2021-05-27 15:28:56 +0530

    refactor: set environment variable for backend URL Port and middleware as httpService

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  postData(data: any , url: any) {
+       return this.http.post(data, url)
+     }
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { HttpService } from './http.service';
+import { environment } from '../../environments/environment'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpService: HttpService) { }
+  registerUser(data: any) {
+    return this.httpService.postData(environment.URL + 'registration', data)
+  }
+
+  login(data: any) {
+    return this.httpService.postData(environment.URL + 'login', data)
+  }
+
+}
