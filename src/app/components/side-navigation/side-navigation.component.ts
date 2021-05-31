498a8c3d2d3241c438d6d9c20632f3284de2743a
gautamb97 <www.gautam971997@gmail.com>
2021-05-31 13:03:44 +0530

    refactor: side navigation baar

---
+++ src/app/components/side-navigation/side-navigation.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-navigation',
+  templateUrl: './side-navigation.component.html',
+  styleUrls: ['./side-navigation.component.scss']
+})
+export class SideNavigationComponent implements OnInit {
+
+  isMenuOpen = false;
+  contentMargin = 240;
+  constructor() { }
+
+  onToolBarToggle() {
+    this.isMenuOpen = !this.isMenuOpen
+    if (!this.isMenuOpen) {
+      this.contentMargin = 50;
+    } else {
+      this.contentMargin = 240;
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
