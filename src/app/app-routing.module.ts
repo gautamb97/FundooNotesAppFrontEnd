e6d672f0356bcd8ec30307b08501861058fbb921
gautamb97 <www.gautam971997@gmail.com>
2021-05-31 12:58:15 +0530

    refactor: route for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from '../app/pages/registration/registration.component';
 import { LoginComponent } from '../app/pages/login/login.component';
 import { ForgotPasswordComponent } from '../app/pages/forgot-password/forgot-password.component';
-import { ResetPasswordComponent } from '../app/pages/reset-password/reset-password.component'
+import { ResetPasswordComponent } from '../app/pages/reset-password/reset-password.component';
+import { DashboardComponent } from '../app/components/dashboard/dashboard.component'
 
 const routes: Routes = [
   { path: '*', component : RegistrationComponent },
   { path: 'register', component : RegistrationComponent },
   { path: 'login', component: LoginComponent },
   { path: 'forgotPassword', component: ForgotPasswordComponent},
-  { path: 'resetPassword/:token', component: ResetPasswordComponent}
+  { path: 'resetPassword/:token', component: ResetPasswordComponent},
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
