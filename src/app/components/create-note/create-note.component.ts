be6fd7550c7785b5f4b9b53eaaa324ed3ddcb49b
gautamb97 <www.gautam971997@gmail.com>
2021-06-04 18:41:28 +0530

    refactor: API integration for create note

--- src/app/components/create-note/create-note.component.ts
+++ src/app/components/create-note/create-note.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from '../create-note/create-note.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-create-note',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateNoteComponent implements OnInit {
   
-  constructor() { }
+  constructor(private snackBar: MatSnackBar,
+              private noteService: UserService) { }
+
+  note = new Note();
 
   ngOnInit(): void {
   }
 
+  message: any
+  data: any
+  error: any
+
+  submit() {
+    const token = localStorage.getItem('token')
+    this.noteService.createNote(this.note, token).subscribe(response => {
+      console.log(response) 
+      this.data = response
+      this.message = this.data.message
+      this.snackBar.open(this.message, '', { duration: 2000 })
+    }, error => {
+      console.log(error)
+      this.error = error.error.err
+      this.message = this.error
+      this.snackBar.open(this.message, '', { duration: 2000 })
+    })
+  }
 }
