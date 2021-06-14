c09c101b7480e6254ad419c22894ff23d5a89ad2
gautamb97 <www.gautam971997@gmail.com>
2021-06-14 15:29:24 +0530

    refactor: use interaction service for resfresh page

--- src/app/components/is-archived/is-archived.component.ts
+++ src/app/components/is-archived/is-archived.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from 'src/app/services/user.service';
+import { InteractionService } from 'src/app/services/interaction.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-is-archived',
@@ -15,10 +16,14 @@ export class IsArchivedComponent implements OnInit {
   message: any
   id: any
   constructor(private service: UserService,
-              private snackBar: MatSnackBar) { }
+              private snackBar: MatSnackBar,
+              private interaction: InteractionService) { }
 
   ngOnInit(): void {
     this.submit()
+    this.interaction.createNoteData$.subscribe(() => {
+      this.submit()
+    })
   }
 
   submit() {
@@ -26,6 +31,26 @@ export class IsArchivedComponent implements OnInit {
       this.data = res
       this.notes = this.data.data
       this.notes = this.notes.reverse()
+    }, error => {
+      console.log(error)
+      this.error = error.error.err
+      this.message = this.error
+    })
+  }
+
+  fetchNoteId(noteId: string) {
+    this.id = noteId
+  }
+
+  unArchiveNote() {
+    console.log(this.id)
+    this.service.unArchiveNote(this.id).subscribe(res => {
+      console.log(res)
+      this.data = res
+      this.notes = this.data.data
+      this.message = this.data.message
+      this.snackBar.open(this.message, '', { duration: 2000 })
+      this.interaction.sendContent('unarchive note')
     }, error => {
       console.log(error)
       this.error = error.error.err
