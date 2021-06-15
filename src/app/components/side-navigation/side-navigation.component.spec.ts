7ec69abd6294c3e894c8452667ca0d32dd216cd3
gautamb97 <www.gautam971997@gmail.com>
2021-06-15 10:41:21 +0530

    test: toggle property

--- src/app/components/side-navigation/side-navigation.component.spec.ts
+++ src/app/components/side-navigation/side-navigation.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SideNavigationComponent } from './side-navigation.component';
 
@@ -8,7 +9,10 @@ describe('SideNavigationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SideNavigationComponent ]
+      declarations: [ SideNavigationComponent ],
+      imports: [
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   });
@@ -19,7 +23,11 @@ describe('SideNavigationComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('side menu property', () => {
+    expect(component.toggle).toBe(undefined)
+  });
 });
